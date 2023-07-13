19529ece593d06a23d157d495a11a628d2a13db3
AGBER,Shima_Emmanuel <agbershima@gmail.com>
2023-07-13 16:44:07 -0700

    printing

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - code starts here
+ *
+ * Return: is 0
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
