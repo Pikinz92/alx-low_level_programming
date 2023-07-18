7185d2d15d21d15e42a87160ea26bb5d4e670167
AGBER,Shima_Emmanuel <agbershima@gmail.com>
2023-07-18 14:00:04 -0700

    print_alphabet error corrected

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,17 @@
+/**
+ * print_alphabet - prints lowercased aphalbets
+ *
+ */
+
 void print_alphabet(void);
-
-print_alphabet()
-{       
-char aphbet[26]={a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
-Int i = 0;
-for(i=0, i<26, i++)
+int _putchar(char c);
+void print_alphabet(void)
 {
-_putchar(char aphbet[i]);
+char aphbet[26] = "abcdefghijklmnopqrstuvwxyz";
+int i = 0;
+for (i = 0; i < 26; i++)
+{
+_putchar(aphbet[i]);
 }
-_putchar(‘\n’);
+_putchar('\n');
 }
-
