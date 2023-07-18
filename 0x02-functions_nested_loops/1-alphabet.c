eb3f695523b6ea85a035579ade373ca98c0dbe7c
AGBER,Shima_Emmanuel <agbershima@gmail.com>
2023-07-18 13:30:33 -0700

    a function that prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+void print_alphabet(void);
+
+print_alphabet()
+{       
+char aphbet[26]={a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
+Int i = 0;
+for(i=0, i<26, i++)
+{
+_putchar(char aphbet[i]);
+}
+_putchar(‘\n’);
+}
+
