52d2fc39467fafb3aba3d215083e10cbf36f32de
AGBER,Shima_Emmanuel <agbershima@gmail.com>
2023-07-18 14:54:02 -0700

    printing lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
+#include "main.h"
 /**
  * print_alphabet - prints lowercased aphalbets
  *
  */
 
 void print_alphabet(void);
-int _putchar(char c);
 void print_alphabet(void)
 {
 char aphbet[26] = "abcdefghijklmnopqrstuvwxyz";
@@ -12,5 +12,6 @@ int i = 0;
 for (i = 0; i < 26; i++)
 {
 _putchar(aphbet[i]);
-};
+}
+_putchar('\n');
 }
