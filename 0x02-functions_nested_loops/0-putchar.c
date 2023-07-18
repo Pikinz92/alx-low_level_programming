25dcb76443c611352ba503af28a5fbdb84821eae
AGBER,Shima_Emmanuel <agbershima@gmail.com>
2023-07-18 12:05:43 -0700

    push main.h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<main.h>
+
+/**
+ * main - code starts here
+ *
+ * Return: 0 is correct
+ *
+ */
+
+int main(void)
+{
+_putchar(95);
+_putchar(112);
+_putchar(117);
+_putchar(116);
+_putchar(99);
+_putchar(104);
+_putchar(97);
+_putchar(114);
+return (0);
+}
