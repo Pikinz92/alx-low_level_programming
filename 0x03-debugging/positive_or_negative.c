bc24c04fa9fa5b80f13e5cee147ed697f0d59983
AGBER,Shima_Emmanuel <agbershima@gmail.com>
2023-07-19 16:52:49 -0700

    a test for a postitive_or_negative()

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *positive_or_negative - entry point of the program
+ *
+ * i: argument used
+ */
+void positive_or_negative(int i)
+{
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+else if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+else
+{
+printf("%d is negative\n", i);
+}
+}
