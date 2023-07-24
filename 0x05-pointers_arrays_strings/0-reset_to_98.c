961023e752dc0582358b6a07d819d057e016fb1c
AGBER,Shima_Emmanuel <agbershima@gmail.com>
2023-07-24 12:55:07 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * reset_to_98 - updates the value it points to to 98
+ *
+ *@n: as parameter
+ *
+ * Return: returns 98
+ *
+ */
+
+void reset_to_98(int *n);
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
+
