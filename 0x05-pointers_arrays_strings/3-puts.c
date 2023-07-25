e4eca1516fbdb81aaa1ff137b948d8522856a396
AGBER,Shima_Emmanuel <agbershima@gmail.com>
2023-07-25 11:18:04 -0700

    printing strings to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ *_puts - strings printout in stdout
+ *
+ *@str: as parameter
+ *
+ */
+
+void _puts(char *str);
+void _puts(char *str)
+{
+int i = 0;
+int j = 0;
+while (*(str + i) != '\0')
+{i++; }
+for (j = 0; j <= i; j++)
+{
+_putchar (*(str + j));
+}
+}
